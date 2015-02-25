71bd270c0372e7e3b9591ccbcb1b679b60013d90
sgoldin <sgoldin@prepgames.com>
2015-02-25 11:44:00 -0500

    continue, not break, and add output.html why not

--- encode64.py
+++ encode64.py
@@ -21,16 +21,16 @@ def main(argv):
       sys.exit()
     elif opt in ('-f', '--file'):
       inputfile = arg
-  
+
   file_dir = os.path.dirname(os.path.abspath(inputfile))
-  
+
   os.chdir(file_dir)
   print 'In dir ' + os.path.abspath(os.curdir)
-  
+
   #print 'Input file is', inputfile
   input_file_name = os.path.basename(inputfile)
   print 'reading ' + input_file_name
-  
+
   f = open(input_file_name,'r')
   filedata = f.read()
   f.close()
@@ -41,19 +41,19 @@ def main(argv):
   for img_src in matches:
     if not os.path.isfile(img_src):
       print 'ERROR: "' + img_src + '" not found. Continuing to next file if exists'
-      break
+      continue
     with open(img_src, "r+") as image_file:
       ext = imghdr.what(image_file)
       encoded_string = base64.b64encode(image_file.read())
-      
+
       print 'Changing src "' + img_src + '" to ' + encoded_string[0:15] + '...'
       new_src = 'data:image/' + ext + ';base64,' + encoded_string
       newdata = filedata.replace(img_src, new_src)
-     
+
       f = open('output.html','w')
       f.write(newdata)
       f.close()
-    
+
   os.chdir(old_dir)
 
 if __name__ == "__main__":
